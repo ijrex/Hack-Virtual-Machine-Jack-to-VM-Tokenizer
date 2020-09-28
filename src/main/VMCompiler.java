1694241e39efce3aa4844d39bd968758a477696d
ijrex <iain.rex@googlemail.com>
2020-09-28 22:09:07 +0100

    feat(tokenizer): Output tokenized files to LinkedLists with tokens

--- src/main/VMCompiler.java
+++ src/main/VMCompiler.java
@@ -1,6 +1,13 @@
 import loadfile.*;
 
+import tokens.token.*;
 import tokenizer.*;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.io.FileWriter;
+import java.io.IOException;
 
 class VMCompiler {
 
@@ -9,7 +16,27 @@ class VMCompiler {
     LoadFiles files = new LoadFiles("../../test-files/ArrayTest", "jack");
 
     Tokenizer tokenizer = new Tokenizer(files);
-    tokenizer.createTokenedFiles();
+    HashMap<String, LinkedList<Token>> tokenizedFiles = tokenizer.createTokenedFiles();
+
+    // Code below this line is temporary and is used to test
+    // the tokenizer
+
+    try {
+
+      for (Map.Entry<String, LinkedList<Token>> tokenizedFile : tokenizedFiles.entrySet()) {
+        FileWriter fileWriter = new FileWriter("../../test-files/ArrayTest/" + tokenizedFile.getKey() + ".test.vm",
+            false);
+
+        for (Token token : tokenizedFile.getValue()) {
+          fileWriter.write(token.getValue() + "\n");
+        }
+        fileWriter.close();
+
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+      System.exit(1);
+    }
 
   }
 }
