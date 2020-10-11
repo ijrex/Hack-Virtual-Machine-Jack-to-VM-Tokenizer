633b4276f5f67cb8efc29118d07b378e1855876b
Iain Rex <iain.rex@ropto.com>
2020-10-11 22:55:49 +0100

    feat(compilation-engine): WIP - Begin planning look ahead functionality

--- src/main/compilationEngine/CompileExpression.java
+++ src/main/compilationEngine/CompileExpression.java
@@ -1,11 +1,17 @@
 package compilationEngine;
 
 import token.*;
+import tokenlib.TokenType;
+
 import java.io.IOException;
+
+import compilationEngine.util.Match;
 
 public class CompileExpression extends Compile {
 
   Compile parameterList;
+
+  Token[] lookAhead;
 
   public CompileExpression(int _tab) {
     super(_tab);
@@ -16,6 +22,29 @@ public class CompileExpression extends Compile {
     switch (pos) {
       case -1:
         return preface(token);
+      case 0:
+        // Define term type
+
+        // If identifier, distinguish using lookahead if
+        // a variable, array entry or subroutine call
+        // - Variable `(...`
+        // - Array `[`
+        // - Subroutine call `.`
+        if (Match.identifier(token))
+          lookAhead = new Token[2];
+
+        if (lookAhead != null) {
+          if (lookAhead[0] == null) {
+            lookAhead[0] = token;
+            return "(looking ahead...)" + token.getValue() + "\n";
+          }
+          if (lookAhead[1] == null) {
+            pos++;
+            lookAhead[1] = token;
+            return lookAhead[0].getValue() + "\n" + token.getValue() + "\n";
+          }
+        }
+
       default:
         return postface();
     }
