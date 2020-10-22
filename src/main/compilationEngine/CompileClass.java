503c5eab859a6d4224b8128f7c1feaafdd371637
Iain Rex <iain.rex@ropto.com>
2020-10-22 21:17:37 +0100

    feat(compilation-engine): Strip back compilation templates

--- src/main/compilationEngine/CompileClass.java
+++ src/main/compilationEngine/CompileClass.java
@@ -17,18 +17,6 @@ public class CompileClass extends Compile {
     wrapperLabel = "class";
   }
 
-  private void setDecType(Token token) throws IOException {
-    if (Match.isSubroutineDec(token)) {
-      compileDec = new CompileSubroutineDec(tab);
-      return;
-    }
-    if (Match.isClassVarDec(token)) {
-      compileDec = new CompileClassVarDec(tab);
-      return;
-    }
-    throw new IOException("ERROR: parsing \"" + token.getValue() + "\"");
-  }
-
   public String handleToken(Token token) throws IOException {
     switch (pos) {
       case -1:
@@ -39,12 +27,6 @@ public class CompileClass extends Compile {
         return parseToken(token, Match.identifier(token));
       case 2:
         return parseToken(token, Match.symbol(token, Symbol.BRACE_L));
-      case 3:
-        // TODO: Handle multiple classVar and subroutine
-        if (compileDec == null)
-          setDecType(token);
-        if (!compileDec.isComplete())
-          return compileDec.handleToken(token);
       default:
         return postface();
     }
