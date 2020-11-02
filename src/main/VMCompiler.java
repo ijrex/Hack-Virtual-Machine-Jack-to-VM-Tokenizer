37d4c9dcaabd411af1dcbdc14c576233c8c31b10
Iain Rex <iain.rex@ropto.com>
2020-11-02 22:34:53 +0000

    feat(tests): If and While statements

--- src/main/VMCompiler.java
+++ src/main/VMCompiler.java
@@ -5,7 +5,7 @@ class VMCompiler {
 
   public static void main(String[] args) {
 
-    String dir = "../../test-files/Grammar/Expressions/Op";
+    String dir = "../../test-files/Grammar/Statements/While";
 
     LoadFiles files = new LoadFiles(dir, "jack");
 
