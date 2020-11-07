f8ac52459dbc366bbe8fb610c5101b0783bfe803
Iain Rex <iain.rex@ropto.com>
2020-11-07 22:00:28 +0000

    feat(tokens): Set max integer value

--- src/main/token/IntConstToken.java
+++ src/main/token/IntConstToken.java
@@ -6,7 +6,21 @@ public class IntConstToken extends Token {
 
   public IntConstToken(String value) {
     super(value);
+
+    try {
+      int intValue = Integer.parseInt(value);
+
+      if (intValue > 32767)
+        throw new NumberFormatException();
+
+    } catch (NumberFormatException e) {
+      System.out.println("ERROR: \"" + value + "\" exceeds max value of 32767");
+      e.printStackTrace();
+      System.exit(1);
+    }
+
     this.type = TokenType.INT_CONST;
+
   }
 
   public String getLabel() {
